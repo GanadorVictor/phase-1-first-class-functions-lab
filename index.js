5119db6476c140987e263f71e51328927fde7ff2
Victor Muturi <muturivictor21@gmail.com>
2023-09-21 14:51:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice `[:2]`; /
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2); 
+}
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier; 
+    };
+}
+
+function fareDoubler(fare) {
+    return fare * 2; }
+
+function fareTripler(fare) {
+    return fare * 3; 
+}
+
+function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers); 
+}
+
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
+module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers,
+    selectingDrivers,
+};
